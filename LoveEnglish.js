2d9d8093395cf5f2a577276e0918799956f8fa55
aninzero <146696557+aninzero@users.noreply.github.com>
2023-10-06 15:18:46 +0800

    Updath.js

--- LoveEnglish.js
+++ LoveEnglish.js
@@ -18,49 +18,30 @@ hostname = revenuecat-cn.fastools.cn
 
 *******************************/
 
-var aninzero = JSON.parse($response.body);
-delete $request.headers["x-revenuecat-etag"];
-delete $request.headers["X-RevenueCat-ETag"];
-    aninzero = {
-  "request_date_ms": 1696509937149,
-  "request_date": "2023-10-05T12:45:37Z",
-  "subscriber": {
-    "non_subscriptions": {
-    },
-    "first_seen": "2023-10-05T12:39:19Z",
-    "original_application_version": "1",
-    "other_purchases": {
-    },
-    "management_url": "https:\/\/apps.apple.com\/account\/subscriptions",
-    "subscriptions": {
-      "igrammar_199_ly_v1": {
-        "original_purchase_date": "2023-10-05T12:43:01Z",
-        "expires_date": "2099-10-12T12:43:00Z",
-        "is_sandbox": false,
-        "refunded_at": null,
-        "store_transaction_id": "190001746507030",
-        "unsubscribe_detected_at": null,
-        "grace_period_expires_date": null,
-        "period_type": "trial",
-        "purchase_date": "2023-10-05T12:43:00Z",
-        "billing_issues_detected_at": null,
-        "ownership_type": "PURCHASED",
-        "store": "app_store",
-        "auto_resume_date": null
-      }
-    },
-    "entitlements": {
-      "default": {
-        "grace_period_expires_date": null,
-        "purchase_date": "2023-10-05T12:43:00Z",
-        "product_identifier": "$rc_annual",
-        "expires_date": "2099-10-12T12:43:00Z"
-      }
-    },
-    "original_purchase_date": "2023-10-05T12:38:34Z",
-    "original_app_user_id": "$RCAnonymousID:9e000cbdd73942ccbe08f755b94f6dcc",
-    "last_seen": "2023-10-05T12:43:47Z"
-  }
+
+const aninzero1 = {};
+const aninzero2 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const name = "default";
+const appid = "igrammer_199_ly_v1";
+
+  
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  chxm1023.headers = $request.headers;
+} else if (aninzero2 && aninzero2.subscriber) {
+  data = {
+ "original_purchase_date": "2022-09-09T09:09:09Z",
+ "purchase_date": "2022-09-09T09:09:09Z",
+ "expires_date": "2099-09-09T09:09:09Z",
+ "store" : "app_store",
+ "ownership_type": "PURCHASED"
+ };
+  aninzero2.subscriber.subscriptions[(appid)] = data
+  aninzero2.subscriber.entitlements[(name)] = JSON.parse(JSON.stringify(data));
+  aninzero2.subscriber.entitlements[(name)].product_identifier = (appid);
+  aninzero1.body = JSON.stringify(aninzero2);
 }
 
-$done({body : JSON.stringify(aninzero)});
+$done(aninzero1);
