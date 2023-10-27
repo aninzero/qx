9df0e8b81fd8d8fc2b93b10ec58f39aefbd982ba
aninzero <146696557+aninzero@users.noreply.github.com>
2023-10-27 20:36:40 +0800

    Create nikec.js

---
+++ nikec.js
@@ -0,0 +1,21 @@
+/*************************************
+
+项目名称：nike抢劵
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/guide-acs\.m\.taobao\.com\/gw\/mtop\.alibaba\.mktview\.trade\.pricecoupon\.queryitem\/1\.0 url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/youpume.js
+
+[mitm]
+hostname = guide-acs.m.taobao.com
+
+*************************************/
+
+
+var aninzero = JSON.parse($response.body);
+
+aninzero.data.model.priceCouponDetailDescVO.canBuy = "true";
+
+$done({ body: JSON.stringify(aninzero)});
