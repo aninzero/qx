e776e6a22b5540dcb1cdd3d7877ff3200b3bc08e
aninzero <146696557+aninzero@users.noreply.github.com>
2023-11-02 15:15:19 +0800

    Update and rename wfyxt.js to wfyxt1.js

--- wfyxt1.js
+++ wfyxt1.js
@@ -7,7 +7,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/iosv2\.cjapi\.5fun\.com\/user\/ios_store_pay_edition\/members_login\/get_member_info url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/wfyxt.js
+^https?:\/\/iosv2\.cjapi\.5fun\.com\/user\/ios_store_pay_edition\/members_login\/get_member_info url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/wfyxt1.js
 
 [mitm]
 hostname = iosv2.cjapi.5fun.com
@@ -15,7 +15,7 @@ hostname = iosv2.cjapi.5fun.com
 *************************************/
 
 
-var aninzero = JSON.parse($response.body);
+var aninzero = $response.body;
 aninzero = "{"error":0,"data":{"error_msg":"","is_success":true,"user_info":{"is_vip":"9A0684792021D73BE42B71491469ADFA","expired_time":2147483647,"member_state":2,"reg_time":"1698559700"},"error_code":"","data_info":[]}}";
 
-$done({ body: JSON.stringify(aninzero)});
+$done({ body: aninzero});
