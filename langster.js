8a95ce2ea528ef3ee4042ee97abb00168d7bf882
aninzero <146696557+aninzero@users.noreply.github.com>
2023-11-07 18:11:47 +0800

    Update langster.js

--- langster.js
+++ langster.js
@@ -4,7 +4,7 @@
 **************************************
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/langster.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/langster.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header  https://raw.githubusercontent.com/aninzero/qx/main/langster.js
 
 [MITM]
 hostname = api.revenuecat.com
