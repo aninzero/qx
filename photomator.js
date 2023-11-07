90094e044682726de5173cfb8f4be6b7aba42bed
aninzero <146696557+aninzero@users.noreply.github.com>
2023-11-07 18:13:41 +0800

    Update photomator.js

--- photomator.js
+++ photomator.js
@@ -4,7 +4,7 @@
 **************************************
 [rewrite_local]
 ^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/photomator.js
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/aninzero/qx/main/photomator.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/aninzero/qx/main/photomator.js
 
 [MITM]
 hostname = api.revenuecat.com
